fcb58239297d6227aec82d3277359217299b1982
James Keicher <jamesdkeicher@gmail.com>
2022-01-24 15:45:06 -0800

    removed console logs and instruction comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css'
-{/* import the zodiac array */}
 import { zodiac } from './data'
 import HeaderTag from './Header/Header'
 import Main from './Main/Main'
 import Footer from './Footer/Footer'
 
-// console.log(zodiac)
+
 function App() {
   return (
     <div className="App">
       <HeaderTag name='Cadillac Jack'/>
-      {/* import and use the Header. Main, and Footer components here */}
-      {/* the Header component needs a string passed in as the `name` prop */}
-      {/* the Footer component needs a year passed in as the `year` prop */}
-      {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
       <Main zodiacList={zodiac} />
       <Footer year='2021-2022' />
     </div>
--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,7 +1,6 @@
 import './ZodiacCard.css'
 
 export default function ZodiacCard(props) {
-  {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
   return (
     <div className='zodiac-card'>
       <img src={`images/${props.name}.png`}>
